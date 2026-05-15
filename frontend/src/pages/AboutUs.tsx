16f6ced1bd0c593c75e74232a8b502688b9e1645
JoeM. <joelmori2020@gmail.com>
2026-05-14 20:28:16 -0500

    Update image source and Google Maps embed link

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -35,7 +35,7 @@ export function AboutUs() {
             <div className="relative">
               <div className="aspect-[4/5] rounded-[4rem] overflow-hidden shadow-2xl skew-x-1 group">
                 <img 
-                  src="https://learn-us-east-1-prod-fleet02-xythos.content.blackboardcdn.com/5e545c6f58bc4/319108170?X-Blackboard-S3-Bucket=learn-us-east-1-prod-fleet01-xythos&X-Blackboard-Expiration=1778187600000&X-Blackboard-Signature=PHN3T6%2BOYE9I8ME0wmzJGGnp35s6a9qM0m%2FUPY6j%2FT4%3D&X-Blackboard-Client-Id=446172&X-Blackboard-S3-Region=us-east-1&response-cache-control=private%2C%20max-age%3D21600&response-content-disposition=inline%3B%20filename%2A%3DUTF-8%27%277536a2ea-c608-4b9a-be21-42b0cc319b45.png&response-content-type=image%2Fpng&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEPH%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJHMEUCIHFR2kkp8t35gc1z95CJheuUvOY8WjzK11zvAXqRlu%2FPAiEA8Ut%2BU3fPqcGyR03WfUEEjXyCxZhEqcGj0EBDVki1aT4qvQUIuv%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAEGgw2MzU1Njc5MjQxODMiDI7wMwKNys0DKt1xvyqRBan3Ymi1ZrtlyR6N4WKZ9%2BOrHUJD4aCmU%2Fnx10CEYjTRtFfQ0qFUwifumzkn1OjzNQ99AjPYdKvc4AyF9LftkJ8rGMX2Y8M2kW18GddNbGWmVR%2ByGzjU5Z3Kk44JUJVFK1B7LCPJa3k1Bt2itIBjAveLSZUSSr5DJqST74MUrJn15Uy4FLMRE8KLogKhLx6nub%2Fg5L65872LfI2xh3gYImVXIYKs3%2FqdkdeHtNfZ5mVYznjGmFrS3gn9vxXBQwOmMQMq8YuitnTadyPPfeZh%2FC2JWR57jbezLLu2fBEUpKOEdizPPHOD9VbabdvFK3pOHT%2BKcY%2FjJDz4BqVTwexfd68S9dEqdJHozm%2FZ8GYY92iQflGSkodo76gdnmf%2FWT08ibFteUsmFZEvHpDZqLeK3UTY8s6vPoyon8S76fKZowFHtE52zjZejuT9WLxRN5OYKiaTHuoMKT428B8nU4e3LAoBB%2FTWnF%2FuEHyD%2F0AjGgpygGqJC%2BJ%2FMP4SCCWrfJxWTmr2GGL18zjJ%2FPdN0oJtWgLQiKKIMtyeBT7VvEG%2BM8ERQ9Te4WFaJSltxLTrY0ACSu6U%2B0aCmplNkzg0eIWnvu%2BtjxWz8nI7mEzcSUt6%2FpRT%2FldENQSGwuNQU6p0RRN%2Fx%2Fq9omI39C14nbwniYCbZdFEwQWm4Q6189OxTwrN6YMHSf1rx8nCi5sU4Lmc50AG4RLyDHlYEgV8jaf%2FOjcIbGiJYxXDHfI04BAENpb2h8iQ6cjx4QILiXFhhvC%2BaR5FYLCWtG3XVkoSkU%2B%2BYYaaTyINMFjvTRVzRz09CexY8HM14b52l7yIdRmBKyJEZlKeT35WWhOxi6dNy7jk6aQIhhDDZza4BArmoc00JWwjagptTzCB%2FvLPBjqxAXgOEkhLKzg6oCbBzR%2F4nn%2BCeeXks%2F1wSAqKQ7761KUUI98ufGdWHugohYiOghcAqmTdJDjNJhohuY7jjkUYuqf%2FpC8EiR2YqumnnuoicNccWfKzeosviEX%2FzBHQfZC862GaK5IaK%2FQ7kAaRy%2Bw1XLLo4uJE4pPiu7UXH%2Fzx2QtzuQ9EmAzl3I%2F%2F%2Fvzi14pwuU3V7BC3y9u42IDSU4CXNyT1tEDhxd2nVqtZSNRZid2BeQ%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20260507T150000Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Credential=ASIAZH6WM4PL4LUOY7LL%2F20260507%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=ddf67fe31d4183c02ef5c6c45d0e4bfe8a85552048860a824b12085117c7f2c3" 
+                  src="https://i.ibb.co/QFZWWC2f/imagen-2026-05-14-183224734.png" 
                   alt="Nuestra tienda" 
                   className="w-full h-full object-cover grayscale group-hover:grayscale-0 transition-all duration-1000"
                   referrerPolicy="no-referrer"
@@ -58,7 +58,7 @@ export function AboutUs() {
               <div className="aspect-video bg-beige-200 rounded-[3rem] overflow-hidden grayscale hover:grayscale-0 transition-all duration-700">
                 {/* Mock Map */}
                 <iframe 
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509374!2d144.95373531531615!3d-37.81720997975171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d4c2b349649%3A0xb6899234e561db5!2sMelbourne%20Central!5e0!3m2!1sen!2sau!4v1567150963539!5m2!1sen!2sau" 
+                  src="https://maps.google.com/maps?q=-8.136355558320641,-79.0357318&output=embed" 
                   className="w-full h-full border-0"
                   loading="lazy"
                 ></iframe>
